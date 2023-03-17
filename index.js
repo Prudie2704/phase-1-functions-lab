fc6d3e9b0795da81f19dea781921103b593a98a7
prudence <prudence.jerono@student.moringaschool.com>
2023-03-17 14:51:42 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    if (number = (`(38-34)*264`))
+    return 1065
+}
+function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    if (distanceFromHqInFeet = (`number/5`))
+    return 213
+}    
+function distanceTravelledInFeet(start, destination) {
+    if (distanceTravelledInFeet = (40, 43))
+    return 792    
+}
+function calculatesFarePrice(start, destination) {
+    it('charges 2 cents per foot if it is between 300 and 1000 and first 100 are free')
+    //total feet travelled is 792, minus the 300 feet,comes to 492 ->2.31
+}
+
